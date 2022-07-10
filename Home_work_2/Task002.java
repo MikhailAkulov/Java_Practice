49327f99eb68fcbfb557a05148975ef3459965d8
Akuloff.M <Akuloff.M@gmail.com>
2022-07-10 18:09:28 +0300

    update Task002 project

--- Home_work_2/Task002.java
+++ Home_work_2/Task002.java
@@ -14,20 +14,20 @@ import java.util.Scanner;
 public class Task002 {
     public static void main(String[] args) {
         Scanner Scanner = new Scanner(System.in);
-        System.out.print("\n\nДо какого треугольного числа будем формировать массив? До: ");
+        System.out.print("\nВведите значение треугольного числа, до которого требуется сформировать массив: ");
         int triangleNum = Scanner.nextInt();
         Scanner.close();
 
-        int[] array = new int[0]; // создаем изначальный массив
+        int[] array = new int[0];                                   // создание первичного массива
 
         for (int i = 1; i <= triangleNum; i++) {
-            array = addMassive(array, i); // расширяем изначальный массив
+            array = addMassive(array, i);                           // расширение первичного массива
             System.out.println(Arrays.toString(array));
         }
 
     }
 
-    public static int Triangle(int num) { // вычисление треугольного числа
+    public static int Triangle(int num) {                           // вычисление треугольного числа
         if (num == 1) {
             return 1;
         }
@@ -35,11 +35,11 @@ public class Task002 {
     }
 
     public static int[] addMassive(int[] array, int newElement) {
-        int[] newArray = new int[array.length + 1]; // объявляем массив размерностью на единицу больше предыдущего
+        int[] newArray = new int[array.length + 1];                 // объявление массива размерностью на единицу больше предыдущего
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
         }
-        newArray[newArray.length - 1] = Triangle(newElement); // добавляем в новый массив новый элемент и возвращаем его
+        newArray[newArray.length - 1] = Triangle(newElement);       // добавление в новый массив нового элемента и возвращение его
         return newArray;
     }
 }
